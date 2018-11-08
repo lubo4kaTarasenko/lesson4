1b9fcf6052a24124d634fcb4f9d742f163efd1a0
luba.t <luba@t.me>
2018-11-08 18:36:30 +0200

    Search without page reload

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-  $(".done").click(function(){
-      
-    alert("hello")
+  $("#search_btn").click(function(){
+    $tasksArr = $(".all_tasks")     
+    searchVal = $("#search_field").val().trim()
+    for(var i = 0; i < $tasksArr.length; i++){
+      var elem = $tasksArr[i]
+      $elemInput = $(elem).find(".task_text")
+      val = $($elemInput).val()
+      if (val.indexOf(searchVal) != -1){
+        $($tasksArr[i]).show()
+      }
+      else{
+        $($tasksArr[i]).hide()
+      }
+    }
   })
 });
