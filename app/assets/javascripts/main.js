0b8871d4e184f4cea53cd9866f7b56d745e6d5a9
luba.t <luba@t.me>
2018-11-13 11:04:22 +0200

    add done-style at tasks.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,4 +20,18 @@ $(document).ready(function(){
     $textArea = t.parents("form").find(".h0")
     $textArea.toggleClass("h150")
   })
+
+
+  $(".task_checkbox").click(function(){
+    checkbox = $(this)
+    $textField = checkbox.parents("form").find(".new_task")
+    $textField.toggleClass("done_task")
+
+  })
+
+  $(":checkbox:checked").each(function(){
+    checkbox = $(this)
+    $textField = checkbox.parents("form").find(".new_task")
+    $textField.toggleClass("done_task") 
+  })
 });
